63bcbc3603805e44df50a6651e686cfcd1db80a4
Ke-Shiuan Lynn <93691564+kslynn128171@users.noreply.github.com>
2022-05-05 21:01:42 +0800

    Add files via upload

--- SingleSpectrumFSA.m
+++ SingleSpectrumFSA.m
@@ -19,13 +19,12 @@ Match_Tol=0.0012; %recommanded values: QTOF: 0.0012, ITFT: 0.0011, QFT: 0.0008
 MaxQualifiedResult=5;
 %---------------------------------------------------------------------------
 % load PubChem database
-load PubChemDatabase
+load PubChemMetabolite
 % initialize parameters
 TINY=0.0;
 note='successful ranked';
 Top2_Score_Diff=-1;
 comp_time=-1;
-is_finished=false;
 % ------------------------------------------------------------------------
 % Preprocessing the m/z values
 nop_org=length(mz); % number of peaks in the spectrum
@@ -87,6 +86,7 @@ nop=length(mz); % number of peaks in the spectrum
 % -----------
 % find possible formula for each m/z values in the spectrum
 idrec=cell(nop,1);
+difmtx=cell(nop,1);
 for j=1:nop
     % adjust match tolerance if the molecular mass is small
     upbd=mz(j)+Match_Tol;
@@ -115,6 +115,7 @@ for j=1:nop
         end
     end
     idrec{j}=idx; % record fomula indices for each peak
+    difmtx{j}=abs(mass(idx)-mz(j)); % record the mass differences of the candidates
 end
 %-----------------------------------------------------------
 % find mother-daughter relations for each precursor formula
@@ -136,6 +137,9 @@ if nof == 1
     return
 end
 totalscore=nof*(nof-1)/2; % the highest score that could possiblly be
+difsum=zeros(noc,1);
+massid=zeros(nop,noc);
+mincnum=zeros(noc,1);
 tic; % resume the timer
 for j=1:noc % for each precursor formula candidate
     vecp=elemnum(idrec{1}(j),:); % element composition in the precursor formula
@@ -149,11 +153,18 @@ for j=1:noc % for each precursor formula candidate
         uid=all(tf,2);
         cnum(k)=sum(uid);
         uidrec{k}=uint32(idx(uid))';
+        if (cnum(k)==1) 
+            massid(k,j)=find(uid);
+        end
     end
-    combnum=prod(cnum(cnum>0)); % total number of combination paths to be examined
-    uidrec=uidrec(2:nop);
-    cnum=cnum(2:nop);
+    if ~isempty(cnum(cnum>0))
+        mincnum(j)=min(cnum(cnum>0));
+    end
+    uidrec=uidrec(2:end); % remove the first record (unused) in the formula indices
+    cnum=cnum(2:end); % remove the first record (unused) in the candidate number
+    massuid=massid(2:end,j); % remove the first record (unused) in the mass indices
     uidrec=cell2mat(uidrec(cnum==1)); % keep only characteristic fragments
+    massuid=massuid(cnum==1);
     nor_cur=length(uidrec); % number of fragments whose formula can be found
     % compute the match score of each combination
     [k,l]=ind2sub([nor_cur,nor_cur],nonzeros(triu(reshape(1:(nor_cur*nor_cur), [nor_cur,nor_cur]),1)));
@@ -161,34 +172,61 @@ for j=1:noc % for each precursor formula candidate
     score(j)=(tempscore+nor_cur)/totalscore;
     precrec(j)=idrec{1}(j);
     nor(j)=nor_cur;
+    % compute the mass differences of this combination
+    tempmtx=difmtx(2:end); % keep the mass differences of the fragments
+    tempmtx=tempmtx(cnum==1); % keep the mass differences of the characteristic fragments
+    difvec=cellfun(@(x,y)x(y),tempmtx,num2cell(massuid)); % extract the mass differences of the characteristic fragments
+    difsum(j)=sum(difvec)+difmtx{1}(j); % add the mass difference of the precursor
 end
 comp_time=toc; % total computation time
+disp(['Computation Time: ',num2str(comp_time),' sec']);
 if all(score==0) % No mother-daughter relation is found for all the precursor formula
     disp('No mother-daughter relation is found for all the precursor formula.');
     disp('--------------------- Identification Result (Mass Match)-----------------')
     idx=idrec{1};
-    massdiff=mass(idx)-precursor;
-    [sorteddiff,sidx]=sort(massdiff);
-    for i=1:length(idx)
+    [sorteddiff,sidx]=sort(difmtx{1}); %mass differences of the precursor formulas
+    for i=1:min(MaxQualifiedResult,length(score))
         fprintf('Formula: %15s, Mass: %10.4f, Difference: %10.5e\n',formula{idx(sidx(i))},mass(idx(sidx(i))),sorteddiff(i));
     end
 else
     idx=idrec{1};
     [ss,sidx]=sort(score,'descend'); % sort the score in a descending order
     Top2_Score_Diff=ss(1)-ss(2); % the difference of the top2 scores
-    uscore=sort(unique(score),'desc'); % score sorting
+    [uscore,uid]=sort(unique(score),'desc'); % score sorting
     bestscore=max(score); % the best score among all formula cansidates
-    CharFragPerc=nor/nof; % percentage of characteristic fragment to total fragments for all formula candidates
-    MaxCF=max(CharFragPerc); % max. percentage of characteristic fragment
-    ranknum=find(uscore==score(1)); % score rank (same scores are ignored)
+    %ranknum=find(uscore==score(1)); % score rank (same scores are ignored)
     ranking=sum(score>=score(1)); % score rank (same scores are accounted for)
-    is_top1_uniq=sum(score==max(score(1)))==1;
-    nor_rec=nor(1);
+    % find out the rank of the correct formula after adjusting for mass difference 
     disp('--------------------- Identification Result (FSA)-----------------')
-    for i=1:length(score)
-        fprintf('Formula: %15s, Mass: %10.4f, Score: %10.4f\n',formula{idx(sidx(i))},mass(idx(sidx(i))),ss(i));
-    end
     disp(['SD of neighboring peak distance = ',num2str(Peak_Dist_SD,'%10.4f')]);
-    disp(['Characteristic fragment ratio = ',num2str(CharFragPerc(sidx(1)),'%10.4f')]);
     disp(['Top-2 score difference = ',num2str(Top2_Score_Diff,'%10.4f')]);
+    rank=1;
+    % disp the top-MaxQualifiedResult precursor formulas
+    for i=1:length(uscore) 
+        tienum=sum(uid==i);
+        if tienum==1 % no candidate has the same score
+            fprintf('Rank#%d, Formula: %15s, Mass: %10.4f, Score: %10.4f\n',rank,formula{idx(sidx(rank))},mass(idx(sidx(rank))),score(sidx(rank)));
+            disp(['Characteristic fragment ratio = ',num2str(nor(sidx(rank))/nof,'%10.4f')]);
+            rank=rank+1;
+            if rank > MaxQualifiedResult
+                break;
+            end
+        else
+            [sdif,didx]=sort(difsum(uid==i)); % sort the mass difference
+            tieidx=find(uid==idrec);
+            tempformula=formula(idx(sidx(tieidx)));
+            tempmass=mass(idx(sidx(tieidx)));
+            for k=1:length(didx)
+                fprintf('Rank#%d, Formula: %15s, Mass: %10.4f, Score: %10.4f, Sum of Mass Differences: %10.4f\n',rank,tempformula{didx(k)},tempmass(didx(k)),score(sidx(rank)),sdif(k));
+                disp(['Characteristic fragment ratio = ',num2str(nor(sidx(rank))/nof,'%10.4f')]);
+                rank=rank+1;
+                if rank > MaxQualifiedResult
+                    break;
+                end
+            end
+            if rank > MaxQualifiedResult
+                break;
+            end
+        end
+    end
 end
